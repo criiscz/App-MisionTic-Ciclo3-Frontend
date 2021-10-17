7755cf5ee0ef5c455be70f92381fe1d965f2f5b1
Cristian Sánchez <criissf15@gmail.com>
2021-10-17 13:30:55 -0500

    Se crea pagina de inicio con peticion a backend

---
+++ src/components/Card.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="card">
+  <p>Producto: {{product_name}}<br>
+  Precio: {{price}}</p>
+  <a href="">Agregar a carrito</a>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props : {
+    product_name: String,
+    price: Number
+  }
+}
+</script>
+
+<style scoped>
+  .card {
+    border-radius: 5px;
+    margin: 5px;
+    width:15em;
+    height: 10em;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    -webkit-box-shadow: 1px 1px 5px;
+    -moz-box-shadow: 1px 1px 5px;
+    box-shadow: 1px 1px 5px;
+  }
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="footer">
+    <img src="https://lancetalent.com/wp-content/uploads/2017/05/ecommerceok.png" alt="Logo tienda">
+    <div class="links">
+      <a href="#">Contacto</a>
+      <a href="#">Acerca de nosotros</a>
+    </div>
+    <div class="social-media">
+      <p>Siguenos</p>
+      <div class="icons">
+        <img src="" class="img-icon" alt="">
+        <img src="" class="img-icon" alt="">
+        <img src="" class="img-icon" alt="">
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "Footer"
+}
+</script>
+
+<style scoped>
+  .footer{
+    display: flex;
+    box-shadow: 1px -1px 20px #aaa;
+    width: 100vw;
+    height: 150px;
+    margin-left: -8px;
+    margin-bottom: 0;
+  }
+
+  .links {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin: 20px;
+    padding: 20px;
+  }
+
+  .social-media {
+    background-color: white;
+    width: 50%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+</style>
